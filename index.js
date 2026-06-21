9ee5dc1980c26e83474179c56ac4a0e221df6d6a
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-21 15:28:41 +0530

    Add command line interface with process.argv

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const { addStudent, viewStudents, deleteStudent, updateStudent } = require('./students/studentManager');
 
-viewStudents();
+const command = process.argv[2];
+const arg1 = process.argv[3];
+const arg2 = process.argv[4];
+const arg3 = process.argv[5];
+
+if (command === 'add') {
+    addStudent(arg1, Number(arg2));
+} else if (command === 'view') {
+    viewStudents();
+} else if (command === 'delete') {
+    deleteStudent(Number(arg1));
+} else if (command === 'update') {
+    updateStudent(Number(arg1), arg2, Number(arg3));
+} else {
+    console.log('Unknown command. Use: add, view, delete, update');
+}
